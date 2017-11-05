54af10e5b2db4d457c861b92561bc9c96018f9db
Ryan Munguia <ryan.munguia92@gmail.com>
2017-11-05 11:42:36 -0800

    adding assignment 8

---
+++ CIS_024C_Python/homeWork/getAge.py
@@ -0,0 +1,4 @@
+
+userAge = int(raw_input("Enter your age: "))
+
+print userAge
---
+++ CIS_024C_Python/homeWork/helperFunctions.py
@@ -0,0 +1,19 @@
+def add(x,y):
+    sum = x + y
+    return sum
+
+def diff(x,y):
+    difference = x - y
+    return difference
+
+def product(x,y):
+    prod = x * y
+    return prod
+
+def greatest(x,y):
+    if x > y:
+        return x
+    else:
+        return y
+
+product(3,2)
