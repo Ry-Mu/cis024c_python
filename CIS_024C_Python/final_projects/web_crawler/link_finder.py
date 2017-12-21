c75100e275264adeb5cfb95a5db42aecfc79bf87
Ryan Munguia <ryan.munguia92@gmail.com>
2017-12-20 22:12:19 -0800

    updates

---
+++ CIS_024C_Python/final_projects/web_crawler/link_finder.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 20 22:09:32 2017
+
+@author: RyanMunguia
+"""
+
+from html.parser import HTMLParser
+from urllib import parse
+
+
+class LinkFinder(HTMLParser):
+
+    def __init__(self, base_url, page_url):
+        super().__init__()
+        self.base_url = base_url
+        self.page_url = page_url
+        self.links = set()
+
+    # When we call HTMLParser feed() this function is called when it encounters an opening tag <a>
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a':
+            for (attribute, value) in attrs:
+                if attribute == 'href':
+                    url = parse.urljoin(self.base_url, value)
+                    self.links.add(url)
+
+    def page_links(self):
+        return self.links
+
+    def error(self, message):
+        pass
