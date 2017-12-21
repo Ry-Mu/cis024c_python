817360ecf5aec865dfd63d4b6f2d9615cc138360
Ryan Munguia <ryan.munguia92@gmail.com>
2017-12-20 22:15:12 -0800

    updates

--- CIS_024C_Python/Crawler/general.py
+++
@@ -1,7 +0,0 @@
-"""
-Each website you crawl is a separate project (folder). New folder = new project/
-Each website you crawl you actually only have two files and that is the queue and the crawled files. And inside
-these files are just a list of links.
-
-The only link that we give the program is a homepage link. It needs a starting point.
-"""
---
+++ CIS_024C_Python/Spider-master/main.py
@@ -0,0 +1,53 @@
+import threading
+from queue import Queue
+from spider import Spider
+from domain import *
+from general import *
+"""
+What this program will do, is basically going to go through all of the pages of a website and gather up all the links.
+This will be useful for those looking to make a site map or a custom search engine. This is a core program.
+"""
+PROJECT_NAME = 'viper-seo'
+HOMEPAGE = 'http://viper-seo.com/'
+DOMAIN_NAME = get_domain_name(HOMEPAGE)
+QUEUE_FILE = PROJECT_NAME + '/queue.txt'
+CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
+NUMBER_OF_THREADS = 8
+queue = Queue()
+Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
+
+
+# Create worker threads (will die when main exits)
+def create_workers():
+    for _ in range(NUMBER_OF_THREADS):
+        t = threading.Thread(target=work)
+        t.daemon = True
+        t.start()
+
+
+# Do the next job in the queue
+def work():
+    while True:
+        url = queue.get()
+        Spider.crawl_page(threading.current_thread().name, url)
+        queue.task_done()
+
+
+# Each queued link is a new job
+def create_jobs():
+    for link in file_to_set(QUEUE_FILE):
+        queue.put(link)
+    queue.join()
+    crawl()
+
+
+# Check if there are items in the queue, if so crawl them
+def crawl():
+    queued_links = file_to_set(QUEUE_FILE)
+    if len(queued_links) > 0:
+        print(str(len(queued_links)) + ' links in the queue')
+        create_jobs()
+
+
+create_workers()
+crawl()
